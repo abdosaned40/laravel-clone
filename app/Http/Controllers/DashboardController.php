a90a23521edc88bf8a28bbe201a6ee0f748b8046
abdosaned40 <153099139+abdosaned40@users.noreply.github.com>
2023-12-19 10:54:58 +0200

    laravel
    fff

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Mail\WelcomeEmail;
+use App\Models\Comment;
+use App\Models\Idea;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+
+class DashboardController extends Controller
+{
+
+    public function index()
+    {
+        $ideas = Idea::orderBy('created_at', 'DESC');
+
+        if (request()->has('search')) {
+            $ideas = $ideas->where('content', 'like', '%' . request()->get('search', '') . '%');
+        }
+
+        return view('dashboard', [
+            'ideas' => $ideas->paginate(5)
+        ]);
+    }
+}
